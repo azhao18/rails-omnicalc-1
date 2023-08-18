cd48e2b996240414e633f6037f2d8ca54204a1b4
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-18 20:29:30 +0000

    square new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "home" })
-  get("/square", { :controller => "pages", :action => "square" })
+  get("/square/new", { :controller => "pages", :action => "square" })
   get("/square/results", { :controller => "pages", :action => "square_results" })
 end
