91bbc72bc1c30050b72f1dbb1401aab6330c4c54
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-18 18:18:16 +0000

    started square

--- app/controllers/concerns/pages_controller.rb
+++ app/controllers/concerns/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def home
     render({ :template => "pages_templates/home"})
   end
+  def square
+    render({ :template => "pages_templates/square"})
+  end
+  
 end
