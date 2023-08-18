810eed77ead8dac7761b927be65e7f90f41dd085
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-18 22:00:12 +0000

    done with random

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,13 @@ class PagesController < ApplicationController
     render({ :template => "pages_templates/square_results"})
   end
 
+  def square_root
+    render({ :template => "pages_templates/square_root"})
+  end
+  def square_root_results
+    render({ :template => "pages_templates/square_root_results"})
+  end
+
   def payment
     render({ :template => "pages_templates/payment"})
   end
@@ -24,5 +31,14 @@ class PagesController < ApplicationController
     render({ :template => "pages_templates/payment_results"})
   end
   
+  def random
+    render({ :template => "pages_templates/random"})
+  end
+  def random_results
+    @minimum = params.fetch("users_number1").to_f
+    @maximum = params.fetch("users_number2").to_f
+    @rand_num = rand(@minimum..@maximum)
+    render({ :template => "pages_templates/random_results"})
+  end
 
 end
