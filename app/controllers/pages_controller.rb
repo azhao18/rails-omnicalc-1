30e216ad97daa1355cbb1bcedbce87cb41374360
azhao18 <136658199+azhao18@users.noreply.github.com>
2023-08-18 21:32:47 +0000

    working on payment results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,17 +2,20 @@ class PagesController < ApplicationController
   def home
     render({ :template => "pages_templates/home"})
   end
+
   def square
     render({ :template => "pages_templates/square"})
   end
   def square_results
     render({ :template => "pages_templates/square_results"})
   end
-  def square_root
-    render({ :template => "pages_templates/square_root"})
+
+  def payment
+    render({ :template => "pages_templates/payment"})
   end
-  def square_root_results
-    render({ :template => "pages_templates/square_root_results"})
+  def payment_results
+    render({ :template => "pages_templates/payment_results"})
   end
   
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get("/", { :controller => "pages", :action => "home" })
   get("/square/new", { :controller => "pages", :action => "square" })
   get("/square/results", { :controller => "pages", :action => "square_results" })
-  
+
   get("/square_root/new", { :controller => "pages", :action => "square_root" })
   get("/square_root/results", { :controller => "pages", :action => "square_root_results" })
+
+  get("/payment/new", { :controller => "pages", :action => "payment" })
+  get("/payment/results", { :controller => "pages", :action => "payment_results" })
 end
